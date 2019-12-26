8c2b60b1afc6c8ad3b0534d471093325259e1aad
sausageRoll <drozdovigor1996@gmail.com>
2019-12-26 14:33:42 +0100

    remove reactive

--- src/main/java/com/redis/service/UpdatesService.java
+++ src/main/java/com/redis/service/UpdatesService.java
@@ -1,53 +1,47 @@
 package com.redis.service;
 
-import com.redis.repository.TimestampRepositoryReactive;
-import com.redis.repository.UpdatesRepositoryReactive;
+import com.redis.repository.TimestampRepository;
+import com.redis.repository.UpdatesRepository;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import reactor.core.publisher.Mono;
-import reactor.util.function.Tuple2;
 
 @RequiredArgsConstructor
 public class UpdatesService {
 
-    private final UpdatesRepositoryReactive updatesRepository;
+    private final UpdatesRepository updatesRepository;
 
-    private final TimestampRepositoryReactive timestampRepository;
+    private final TimestampRepository timestampRepository;
 
-    public Mono<Boolean> addNewUpdates(String id, Long timestamp, List<String> updates) {
-        return Mono.zip(
-                updatesRepository.addNewUpdates(id, timestamp, updates),
-                timestampRepository.addNewTimestamp(id, timestamp)
-        )
-                .map(Tuple2::getT1);
+    public boolean addNewUpdates(String id, Long timestamp, List<String> updates) {
+        boolean result = updatesRepository.addNewUpdates(id, timestamp, updates);
+        timestampRepository.addNewTimestamp(id, timestamp);
+        return result;
     }
 
-    public Mono<Boolean> deleteOldUpdates(String id) {
-        return updatesRepository.getById(id)
-                .flatMap(map -> {
-                    Long max = map.keySet()
-                            .stream()
-                            .max(Comparator.comparingLong(l -> l))
-                            .orElseThrow();
-                    List<Long> timestamps = map.keySet()
-                            .stream()
-                            .filter(timestamp -> timestamp < max)
-                            .collect(Collectors.toList());
-                    return timestampRepository.overrideOldValue(id, max)
-                            .zipWith(updatesRepository.deleteTimestamps(id, timestamps)
-                                    .map(deleted -> timestamps.size() == deleted))
-                            .map(tuple -> tuple.getT1() && tuple.getT2());
-                });
+    public Boolean deleteOldUpdates(String id) {
+        Map<Long, List<String>> byId = updatesRepository.getById(id);
+        Long max = byId.keySet()
+                .stream()
+                .max(Comparator.comparingLong(l -> l))
+                .orElseThrow();
+        List<Long> timestamps = byId.keySet()
+                .stream()
+                .filter(timestamp -> timestamp < max)
+                .collect(Collectors.toList());
+        boolean firstResult = timestampRepository.overrideOldValue(id, max);
+        boolean secondResult = updatesRepository.deleteTimestamps(id, timestamps) == timestamps.size();
+        return firstResult && secondResult;
     }
 
-    public Mono<Long> getOldest() {
-        return timestampRepository.getAll()
-                .map(list -> list.stream().max(Long::compareTo).orElseThrow());
+    public Long getOldest() {
+        List<Long> list = timestampRepository.getAll();
+        return list.stream().max(Long::compareTo).orElseThrow();
     }
 
-    public Mono<String> getDbState() {
+    public String getDbState() {
         return timestampRepository.info();
     }
 
